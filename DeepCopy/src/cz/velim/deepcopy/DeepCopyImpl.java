9c2191f722b58dea05a47df9e04ec33945cd20d4
Michal Velek <mvelek@gmail.com>
2013-08-01 20:36:44 +0200

    untrack DeepCopy

--- DeepCopy/src/cz/velim/deepcopy/DeepCopyImpl.java
+++ DeepCopy/src/cz/velim/deepcopy/DeepCopyImpl.java
@@ -20,14 +20,19 @@ public class DeepCopyImpl implements Cloneable {
 	 * @throws CloneNotSupportedException
 	 */
 	public static void main(String[] args) throws CloneNotSupportedException {
+
 		DeepCopyImpl dci = new DeepCopyImpl();
 		DeepCopyImpl dciCloned = (DeepCopyImpl) dci.clone();
+
 		dci.classA.memberSetter(1, 1);
 		dciCloned.classA.memberSetter(3, 3);
+
 		System.out.println("original object: a =  " + dci.classA.getA()
 				+ ", b =  " + dci.classA.getB() + ", p =  " + dci.p);
-		System.out.println("cloned object: a =  " + dciCloned.classA.getA()
-				+ ", b =  " + dciCloned.classA.getB() + ", p =  " + dciCloned.p);
+		System.out
+				.println("cloned object: a =  " + dciCloned.classA.getA()
+						+ ", b =  " + dciCloned.classA.getB() + ", p =  "
+						+ dciCloned.p);
 	}
 
 }
